a9c42e7d6a72cb8ba04aa1c59ef5fa127759a114
Md. Moniruzzaman <monir.jbt@gmail.com>
2022-12-26 03:06:56 +0600

    Added Retweets

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -7,7 +7,7 @@ export class TweetService {
   constructor(private configService: ConfigService) {}
 
   async getTweet(tweetID: string) {
-    const res = await axios({
+    const quotes = await axios({
       method: 'GET',
       baseURL: `https://api.twitter.com/2/tweets/${tweetID}/quote_tweets?expansions=author_id&tweet.fields=created_at&user.fields=created_at`,
       headers: {
@@ -17,9 +17,20 @@ export class TweetService {
       },
     });
 
+    const retweets = await axios({
+      method: 'GET',
+      baseURL: `https://api.twitter.com/2/tweets/${tweetID}/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at`,
+      headers: {
+        Authorization: `Bearer ${this.configService.get(
+          'TWITTER_BEARER_TOKEN',
+        )}`,
+      },
+    });
+
     return {
       status: 200,
-      quotes: res.data,
+      quotes: quotes.data,
+      retweets: retweets.data,
     };
   }
 }
