ceecda2d89d9f1b87814fb65d1775c7e27a6ed8c
Md. Moniruzzaman <monir.jbt@gmail.com>
2022-12-26 04:40:18 +0600

    Milestone 3 Modifications

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -9,7 +9,7 @@ export class TweetService {
   async getTweet(tweetID: string) {
     const quotes = await axios({
       method: 'GET',
-      baseURL: `https://api.twitter.com/2/tweets/${tweetID}/quote_tweets?expansions=author_id&tweet.fields=created_at&user.fields=created_at`,
+      baseURL: `https://api.twitter.com/2/tweets/${tweetID}/quote_tweets?expansions=author_id&tweet.fields=created_at&user.fields=created_at&max_results=100`,
       headers: {
         Authorization: `Bearer ${this.configService.get(
           'TWITTER_BEARER_TOKEN',
@@ -19,7 +19,7 @@ export class TweetService {
 
     const retweets = await axios({
       method: 'GET',
-      baseURL: `https://api.twitter.com/2/tweets/${tweetID}/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at`,
+      baseURL: `https://api.twitter.com/2/tweets/${tweetID}/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at&max_results=100`,
       headers: {
         Authorization: `Bearer ${this.configService.get(
           'TWITTER_BEARER_TOKEN',
@@ -27,10 +27,23 @@ export class TweetService {
       },
     });
 
+    const userIds = [];
+
+    for (const user of quotes.data.data) {
+      userIds.push(user.author_id);
+    }
+
+    for (const user of quotes.data.data) {
+      userIds.push(user.id);
+    }
+
+    const unique_userID = [...new Set(userIds)];
+
     return {
       status: 200,
       quotes: quotes.data,
       retweets: retweets.data,
+      unique_userID,
     };
   }
 }
