a5874a84c5a0522932e252e4c0acdcf5a0a55c03
Md. Moniruzzaman <monir.jbt@gmail.com>
2022-12-26 16:22:44 +0600

    Added Maximum Possible Metrics

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -7,42 +7,114 @@ export class TweetService {
   constructor(private configService: ConfigService) {}
 
   async getTweet(tweetID: string) {
-    const quotes = await axios({
-      method: 'GET',
-      baseURL: `https://api.twitter.com/2/tweets/${tweetID}/quote_tweets?expansions=author_id&tweet.fields=created_at&user.fields=created_at&max_results=100`,
-      headers: {
-        Authorization: `Bearer ${this.configService.get(
-          'TWITTER_BEARER_TOKEN',
-        )}`,
-      },
-    });
+    const headers = {
+      Authorization: `Bearer ${this.configService.get('TWITTER_BEARER_TOKEN')}`,
+    };
 
-    const retweets = await axios({
-      method: 'GET',
-      baseURL: `https://api.twitter.com/2/tweets/${tweetID}/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at&max_results=100`,
-      headers: {
-        Authorization: `Bearer ${this.configService.get(
-          'TWITTER_BEARER_TOKEN',
-        )}`,
+    //RETWEETS STARTS
+    const allRetweets = [];
+    let rCount = 0;
+
+    async function callRetweets(token: string) {
+      rCount++;
+      const retweets = await axios({
+        method: 'GET',
+        baseURL: `https://api.twitter.com/2/tweets/${tweetID}/retweeted_by?user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=created_at&max_results=100${
+          token ? `&pagination_token=${token}` : ''
+        }`,
+        headers,
+      });
+
+      const resRetweets = retweets.data.data;
+
+      if (resRetweets && resRetweets.length > 0) {
+        allRetweets.push(...resRetweets);
+
+        const nextToken = retweets.data.meta.next_token;
+
+        if (nextToken && rCount < 30) {
+          await callRetweets(nextToken);
+        }
+
+        rCount = 0;
+        return;
+      }
+
+      return;
+    }
+
+    await callRetweets(undefined);
+
+    //RETWEETS ENDS
+
+    //QUOTES START
+    const allQuotes = [];
+    let qCount = 0;
+
+    async function callQuotes(token: string) {
+      qCount++;
+      const quotes = await axios({
+        method: 'GET',
+        baseURL: `https://api.twitter.com/2/tweets/${tweetID}/quote_tweets?expansions=author_id&tweet.fields=created_at&user.fields=created_at&max_results=100${
+          token ? `&pagination_token=${token}` : ''
+        }`,
+        headers,
+      });
+
+      console.log(quotes.data.data.length);
+
+      const resQuotes = quotes.data.data;
+
+      if (resQuotes && resQuotes.length > 0) {
+        allQuotes.push(...resQuotes);
+
+        const nextToken = quotes.data.meta.next_token;
+
+        if (nextToken && qCount < 30) {
+          await callQuotes(nextToken);
+        }
+
+        qCount = 0;
+        return;
+      }
+
+      return;
+    }
+
+    await callQuotes(undefined);
+
+    //QUOTES END
+
+    const retweets = {
+      data: allRetweets,
+      meta: {
+        result_count: allRetweets.length,
       },
-    });
+    };
+
+    const quotes = {
+      data: allQuotes,
+      meta: {
+        result_count: allQuotes.length,
+      },
+    };
 
     const userIds = [];
 
-    for (const user of quotes.data.data) {
-      userIds.push(user.author_id);
+    for (const user of allRetweets) {
+      userIds.push(user.id);
     }
 
-    for (const user of quotes.data.data) {
-      userIds.push(user.id);
+    for (const user of allQuotes) {
+      userIds.push(user.author_id);
     }
 
     const unique_userID = [...new Set(userIds)];
 
     return {
       status: 200,
-      quotes: quotes.data,
-      retweets: retweets.data,
+      retweets,
+      quotes,
       unique_userID,
     };
   }
